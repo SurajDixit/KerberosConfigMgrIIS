46d45096ea14c0a228d4db1a54dcbf4623b3d1e6
Suraj Dixit <surajd1969@gmail.com>
2018-01-11 07:47:18 +0530

    Final

--- KerberosConfigMgr/Properties/AssemblyInfo.cs
+++ KerberosConfigMgr/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsFormsApplication2")]
+[assembly: AssemblyTitle("KerberosConfigMgr")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApplication2")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("KerberosConfigMgr")]
+[assembly: AssemblyCopyright("Copyright ©  Suraj Dixit")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
