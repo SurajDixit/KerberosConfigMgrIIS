14f7f7248528d5f9aaedddf62aafd4bcc455ea3d
Suraj Dixit <surajd1969@gmail.com>
2018-03-28 23:50:37 +0530

    Assembly Description Change - v2.0

--- KerberosConfigMgrIIS/Properties/AssemblyInfo.cs
+++ KerberosConfigMgrIIS/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("KerberosConfigMgrIIS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("KerberosConfigMgrIIS")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("KerberosConfigMgrIIS")]
